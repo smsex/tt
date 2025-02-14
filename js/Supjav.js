91968f1c34e9dfde6c537e4c10ac3b31180782a0
smsex <smsex9@gmail.com>
2025-02-14 22:53:35 +0800

    init

---
+++ js/Supjav.js
@@ -0,0 +1,23 @@
+var rule = {
+    title:'Missav',
+    host:'https://supjav.com',
+	searchUrl:'/cn/search/**',
+    url:'/zh/fyclass/page/fypage',
+    headers:{
+        'User-Agent':'PostmanRuntime/7.37.3'
+    },
+    timeout:5000,
+    class_name:'热门&新作上市&无码流出&SIRO&LUXU&GANA&PRESTIGE&S-CUTE&ARA&FC2&麻豆传媒&本月热门',//静态分类名称拼接
+    class_url:'popular&release&uncensored-leak&siro&luxu&gana&maan&scute&ara&fc2&madou&monthly-hot',//静态分类标识拼接
+    limit:5,
+    play_parse:true,
+    lazy:'',
+    // 推荐:".grid.grid-cols-2&&.thumbnail.group;.my-2.text-sm.text-nord4.truncate&&Textt;.relative.aspect-w-16.aspect-h-9&&a&&img&&data-src;a&&href",
+    一级:'.posts.clearfix&&.post;a&&img&&alt;a&&img&&data-src;.con&&.meta&&Text;a&&href',
+    二级:'*',
+	搜索:'.grid.grid-cols-2.gap-5&&.thumbnail.group;.relative.aspect-w-16&&a&&img&&alt;.relative.aspect-w-16&&a&&img&&data-src;.absolute.bottom-1&&Text;a&&href',
+	searchable:1,//是否启用全局搜索,
+    quickSearch:1,//是否启用快速搜索,
+    filterable:0,//是否启用分类筛选,
+}
+
